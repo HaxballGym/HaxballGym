e49460d4f7129fc3336d9a6e2839dd9d6e3c2106
Wazarr <jeje_04@live.com>
2022-06-27 23:20:33 +0200

    add UI

--- haxballgym/game/modules/game/game_renderer.py
+++ haxballgym/game/modules/game/game_renderer.py
@@ -1,4 +1,4 @@
-from ursina import Ursina, window, camera, Vec2, destroy
+from ursina import Ursina, window, camera, Vec2, destroy, application
 
 
 class GameRenderer(object):
@@ -8,6 +8,8 @@ class GameRenderer(object):
         self.disc_entities = []
         self.segment_entities = []
         self.background_entities = []
+        self.UI_strings = []
+        self.UI_fixed_entities = []
 
     def start(self) -> None:
 
@@ -18,11 +20,13 @@ class GameRenderer(object):
             self.app = Ursina(
                 title="HaxballGym",
             )
+            self.UI_fixed_entities = self.game.score.get_fixed_entities()
 
         window.exit_button.visible = False
         camera.orthographic = True
         camera.position = Vec2(0, 0)
         camera.fov = 550
+        application.fonts_folder = f"haxballgym/game/modules/game/fonts/"
 
         self.disc_entities = [
             disc.get_entity() for disc in self.game.stadium_game.discs
@@ -31,6 +35,7 @@ class GameRenderer(object):
             segment.get_entity() for segment in self.game.stadium_game.segments
         ]
         self.background_entities = self.game.stadium_game.background.get_entities()
+        self.UI_strings = self.game.score.get_string_entities()
 
         self.app.step()
 
@@ -38,11 +43,20 @@ class GameRenderer(object):
         for entity, game_disc in zip(self.disc_entities, self.game.stadium_game.discs):
             entity.position = game_disc.position
             entity.texture = "circle_outlined"
+
+        if self.UI_strings[0].text != self.game.score.get_score_string():
+            self.UI_strings[0].text = self.game.score.get_score_string()
+        if self.UI_strings[1].text != self.game.score.get_time_string():
+            self.UI_strings[1].text = self.game.score.get_time_string()
+
         self.app.step()
 
     def stop(self):
         # destroy all entities created by this renderer
         for entity in (
-            self.disc_entities + self.segment_entities + self.background_entities
+            self.disc_entities
+            + self.segment_entities
+            + self.background_entities
+            + self.UI_strings
         ):
             destroy(entity)
--- haxballgym/game/modules/game/game_score.py
+++ haxballgym/game/modules/game/game_score.py
@@ -4,7 +4,12 @@ from haxballgym.game.common_values import (
     TEAM_SPECTATOR_ID,
     GAME_STATE_KICKOFF,
     GAME_STATE_PLAYING,
+    TEAM_RED_COLOR,
+    TEAM_BLUE_COLOR,
 )
+from haxballgym.game.objects.base import PhysicsObject
+
+from ursina import *
 
 
 class GameScore(object):
@@ -71,3 +76,77 @@ class GameScore(object):
             return TEAM_BLUE_ID
         else:
             return TEAM_SPECTATOR_ID
+
+    def get_time_string(self) -> str:
+        return f"{int(self.time / 60):02d}:{int(self.time % 60):02d}"
+
+    def get_score_string(self) -> str:
+        return f"{self.red} - {self.blue}"
+
+    def get_time_entity(self) -> Entity:
+        text_time = self.get_time_string()
+        text_time_width = Text.get_width(text_time, font="fonts/arial-black.ttf")
+        print(text_time_width)
+
+        time_text_entity = Text(
+            position=Vec2(0.3 - 1.5 * text_time_width / 2, 0.5 - Text.size * 1.35),
+            origin=Vec2(0, 0),
+            size=1.25 * Text.size,
+            text=self.get_time_string(),
+            font="fonts/arial-black.ttf",
+        )
+
+        return time_text_entity
+
+    def get_fixed_entities(self) -> Entity:
+        background_score = Entity(
+            parent=camera.ui,
+            position=Vec3(0, 0.5 - Text.size * 1.25, 1),
+            scale=(0.6, Text.size * 2.5),
+            model=Quad(
+                aspect=(0.6 / Text.size * 2.5),
+                radius=0.1,
+            ),
+            color=PhysicsObject.parse_color_entity("1A2125"),
+        )
+
+        red_score_square = Entity(
+            parent=camera.ui,
+            position=Vec2(-0.25, 0.5 - Text.size * 1.25),
+            scale=Text.size * 1.5,
+            model=Quad(
+                aspect=1,
+                radius=0.1,
+            ),
+            color=PhysicsObject.parse_color_entity(TEAM_RED_COLOR),
+        )
+
+        blue_score_square = Entity(
+            parent=camera.ui,
+            position=Vec2(-0.10, 0.5 - Text.size * 1.25),
+            scale=Text.size * 1.5,
+            model=Quad(
+                aspect=1,
+                radius=0.1,
+            ),
+            color=PhysicsObject.parse_color_entity(TEAM_BLUE_COLOR),
+        )
+
+        return [background_score, red_score_square, blue_score_square]
+
+    def get_string_entities(self) -> Entity:
+
+        score_text_entity = Text(
+            position=Vec2(-0.175, 0.5 - Text.size * 1.35),
+            origin=Vec2(0, 0),
+            size=1.25 * Text.size,
+            text=self.get_score_string(),
+            font="fonts/arial-black.ttf",
+        )
+
+        time_text_entity = self.get_time_entity()
+
+        return [
+            score_text_entity,
+            time_text_entity,
+        ]
